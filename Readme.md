37ec388b92a5e314272b39a76ea7ac22723dff51
Nathan Henrique Bezerra Feitoza <1171139648@acd.grupotiradentes.com>
2018-08-08 20:42:30 -0300

    Alterações para o projeto

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 Repositório para testes com Node.JS e alguns scripts Shell/Node para automatizar tarefas manuais.
+npm install dblite 
+npm install --save socket.io
+
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,67 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+router = express.Router(),
+http = require('http').Server(express),
+io = require('socket.io')(http);
+
+/* Websocket */
+http.listen(1212, function(){
+  io.on("connection", function(c){
+	  console.log('Alguem conectou', 'ID:', c.id);
+	  c.emit('teste','Mensagem de teste');
+  })
+  /*io.on("connection", function (client) {
+	  client.on("join", function(name){
+		console.log("Joined: " + name);
+		clients[client.id] = name;
+		client.emit("update", "You have connected to the server.");
+		client.broadcast.emit("update", name + " has joined the server.")
+	  });
+
+	  client.on("send", function(msg){
+		console.log("Message: " + msg);
+		client.broadcast.emit("chat", clients[client.id], msg);
+	  });
+
+	  client.on("disconnect", function(){
+		console.log("Disconnect");
+		io.emit("update", clients[client.id] + " has left the server.");
+		delete clients[client.id];
+	  });
+  });*/
+});
 
 /* GET home page. */
-const rgex = /conectar\/[a-zA-z0-9@#]/;
+const rgex = /app\/[a-zA-z0-9@#]/;
+//chave 38036042c461dbb3c7dacdf58072cb27
 router.get(rgex, function(req, res, next) {
-	//const sqlite3 = require('sqlite3').verbose();
+	var dblite = require('dblite');
+	db = dblite('banco/servidor.db');
 	const chave_api = '123456';
 	var acesso = req.url;
 	acesso = acesso.split('/');
 	var chave_enviada = acesso[acesso.length-1];
-	if(chave_enviada == chave_api) {
-		res.send('Iguais');
-	} else {
-		res.send('NO');
+	
+	function init(id_aparelho) {
+		
+		res.send('iniciado');
 	}
-	res.send('a');
+	
+	function erro(msg) {
+		res.send(msg);
+		return false;
+	}
+	
+	db.query('SELECT * FROM usuarios where chave_api = ?',[chave_enviada], function(err, rows) {
+	  if(err) {
+		  console.log(err);
+		  return erro('Erro no banco de dados: '+JSON.stringify(err));
+		  //return false;
+	  }
+	  if(rows.length == 0) {
+		  return erro('Chave de API inválida');
+	  }
+	  init(rows);
+	});
   //res.render('index', { title: 'Express' });
 });
 
