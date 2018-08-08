7b01299d6379397a1b38c35d57187c711004dde9
Nathan Henrique Bezerra Feitoza <1171139648@acd.grupotiradentes.com>
2018-08-07 21:25:07 -0300

    Adicionada coisas para o projeto

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const rgex = /conectar\/[a-zA-z0-9@#]/;
+router.get(rgex, function(req, res, next) {
+	//const sqlite3 = require('sqlite3').verbose();
+	const chave_api = '123456';
+	var acesso = req.url;
+	acesso = acesso.split('/');
+	var chave_enviada = acesso[acesso.length-1];
+	if(chave_enviada == chave_api) {
+		res.send('Iguais');
+	} else {
+		res.send('NO');
+	}
+	res.send('a');
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
